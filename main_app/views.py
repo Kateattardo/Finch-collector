a7e07cd401ff61ff4b0a5e3708d5f32a0c5f68c6
Kateattardo <youremail@domain.com>
2023-09-14 09:03:18 -0400

    finch lab 3 & 4

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
 finchs = [
-  {'breed': 'goldfinch', 'description': 'gold with some balck'},
-  {'breed': 'housefinch', 'description': 'red and lightbrown'},
+  {'breed': 'American Goldfinch', 'description': 'vibrant yellow with some balck markings'},
+  {'breed': 'Housefinch', 'description': 'red and lightbrown'},
+  {'breed': 'Strawberry finch', 'description': 'vibrant red with white and balck markings'},
 ]
 
 # Create your views here.
@@ -14,5 +18,37 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def finch_index(request):
+def finchs_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches})
+
+def finchs_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
+    return render(request, 'finchs/detail.html', {
+    'finch': finch, 'feeding_form': feeding_form
+  })
+  
+    return render(request, 'finches/detail.html', { 'finch': finch})
+    return redirect('detail', finch_id=finch_id)
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finchs/{finch_id}'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description',]
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finchs'
